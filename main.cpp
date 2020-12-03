b75ab7b499b56c382e4de760d564b55b29788ae5
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-12-02 21:50:08 -0500

    bit more messing around

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string>
 using namespace std;
 
 int main(int argc, char ** argv) {
@@ -7,13 +8,23 @@ int main(int argc, char ** argv) {
     cbreak();
     /* raw(); */
     noecho();
+    start_color();
     
-    WINDOW * win = newwin(10,20,10,10); 
-    
+    init_pair(1, COLOR_CYAN, COLOR_BLACK); 
+    init_pair(2, COLOR_BLACK, COLOR_CYAN); 
+
+    int height, width;
+    getmaxyx(stdscr, height, width);
+
+    WINDOW * win = newwin(10,20,5,10); 
+    WINDOW * todo_win = newwin(20,20,5,35);
+    WINDOW * bottombar = newwin(1,width,height-1,0); 
     refresh();
     
     int x, y;
     x = y = 0;
+
+    string todo_list[3] = {"Contemplate life", "Question life", "Ponder about life"};
 
     while (true) {
         int ch = getch();
@@ -35,12 +46,26 @@ int main(int argc, char ** argv) {
         } 
         if (ch == 113) break; // q for quit
 
-        move(y,x);
 
         box(win, 0, 0);
+        wattron(win,COLOR_PAIR(1));
+        wattron(win, A_BOLD);
         mvwprintw(win, 0, 1, "lmao");
+        wattroff(win, A_BOLD);
+        wattroff(win,COLOR_PAIR(1));
+        mvwprintw(win, 1, 2, "poopoopeepee");
         wrefresh(win);
 
+        for (int i = 0; i < sizeof(todo_list)/sizeof(string); i++) {
+            mvwprintw(todo_win, 2*i, 0, todo_list[i].c_str());
+        }
+        wrefresh(todo_win);
+
+        wbkgd(bottombar, COLOR_PAIR(2));        
+        mvwprintw(bottombar, 0, 2, "BOTTOM TEXT");
+        wrefresh(bottombar);
+
+        move(y,x);
         refresh();
         /* clear(); */
     }
